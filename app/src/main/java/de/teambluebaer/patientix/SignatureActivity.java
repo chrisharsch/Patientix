14e64683a4b9a45c951138213e1a83da624b22a1
crunkchilla <chris@141.19.196.253>
2015-05-08 10:40:43 +0200

    overview edited

--- app/src/main/java/de/teambluebaer/patientix/SignatureActivity.java
+++ app/src/main/java/de/teambluebaer/patientix/SignatureActivity.java
@@ -1,10 +1,9 @@
 package de.teambluebaer.patientix;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.patientix.maren.patientix.R;
 
@@ -13,6 +12,9 @@ public class SignatureActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_signature);
     }
 
