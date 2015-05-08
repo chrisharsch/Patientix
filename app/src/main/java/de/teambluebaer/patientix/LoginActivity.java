658f923e21dbefa4925869da48149c34e732663b
Maren <mary_dietrich@gmx.de>
2015-05-08 12:31:29 +0200

    Login function

--- app/src/main/java/de/teambluebaer/patientix/LoginActivity.java
+++ app/src/main/java/de/teambluebaer/patientix/LoginActivity.java
@@ -2,6 +2,7 @@ package de.teambluebaer.patientix;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -9,16 +10,19 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.patientix.maren.patientix.R;
 
 
 public class LoginActivity extends Activity {
 
-    DatabaseHelper myDb;
     private Button buttonLogin;
-    private EditText editName;
-    private EditText editPassword;
+    private EditText editName = null;
+    private EditText editPassword = null;
+    private TextView attempts;
+    int counter = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +35,6 @@ public class LoginActivity extends Activity {
 
         setContentView(R.layout.activity_login);
 
-        myDb = new DatabaseHelper(this);
-
         // Keyboard open when touch editfield
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
@@ -44,11 +46,33 @@ public class LoginActivity extends Activity {
             @Override
             public void onClick(View v) {
                 flashButtonsmall(buttonLogin);
+                login(v);
                 Intent intentFormActivity = new Intent(LoginActivity.this, StartActivity.class);
                 startActivity(intentFormActivity);
                 finish();
             }
         });
+    }
+
+    public void login(View view) {
+
+        buttonLogin.setEnabled(true);
+
+        if (editName.getText().toString().equals("MTRA") &&
+                editPassword.getText().toString().equals("MTRA")) {
+            Toast.makeText(getApplicationContext(), "Login...", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Passwort falsch",
+                    Toast.LENGTH_SHORT).show();
+            buttonLogin.setEnabled(false);
+            /*
+            attempts.setBackgroundColor(Color.RED);
+            counter--;
+            attempts.setText(Integer.toString(counter));
+            if(counter==0){
+                login.setEnabled(false);
+            } */
+        }
     }
 
     public void flashButtonsmall(final Button myBtnToFlash) {
