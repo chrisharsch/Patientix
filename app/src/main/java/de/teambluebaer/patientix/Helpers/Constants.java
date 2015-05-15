faf80b867367a427f62c7bcfed26c1789b27778d
crunkchilla <chris@141.19.196.253>
2015-05-15 13:34:31 +0200

    Delete unused classes

--- app/src/main/java/de/teambluebaer/patientix/Helpers/Constants.java
+++
@@ -1,8 +0,0 @@
-package de.teambluebaer.patientix.helpers;
-
-public class Constants {
-
-	public static final String FIRST_COLUMN="First";
-	public static final String SECOND_COLUMN="Second";
-
-}
--- app/src/main/java/de/teambluebaer/patientix/Helpers/Flasher.java
+++
@@ -1,7 +0,0 @@
-package de.teambluebaer.patientix.helpers;
-
-/**
- * Created by chris on 13.05.15.
- */
-public class Flasher {
-}
