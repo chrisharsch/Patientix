ca62e18d2c7ca38e48151c73d75e485395a3e62f
crunkchilla <chris@141.19.196.253>
2015-05-18 20:38:08 +0200

    edited update function of buttonUpdate von StartActivity

--- app/src/main/java/de/teambluebaer/patientix/activities/LoginActivity.java
+++ app/src/main/java/de/teambluebaer/patientix/activities/LoginActivity.java
@@ -2,7 +2,10 @@ package de.teambluebaer.patientix.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -11,12 +14,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.security.MessageDigest;
 import java.util.ArrayList;
 
 import de.teambluebaer.patientix.R;
 import de.teambluebaer.patientix.helper.Flasher;
+import de.teambluebaer.patientix.helper.RestfulHelper;
 
 public class LoginActivity extends Activity {
 
@@ -52,7 +57,7 @@ public class LoginActivity extends Activity {
         Flasher.flash(buttonLogin, "1x3");
 
         //create parameterMap to add parameters of the request
-        /*
+
         ConnectivityManager connManager = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
         NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
@@ -67,9 +72,10 @@ public class LoginActivity extends Activity {
             if (responseCode == 200) {
                 Toast.makeText(LoginActivity.this, "Login erfolgreich!", Toast.LENGTH_SHORT).show();
                 Log.d("Login successful: ", responseCode + "");
-              */  Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                Intent intent = new Intent(LoginActivity.this, FormActivity.class);
                 startActivity(intent);
-          /*  } else {
+                //finish();
+            } else {
                 Log.d("ResponseCode: ", responseCode + "");
                 checkResponseCode();
             }
@@ -77,7 +83,7 @@ public class LoginActivity extends Activity {
         } else {
             Toast.makeText(this, "WiFi ist abgeschaltet", Toast.LENGTH_LONG).show();
         }
-*/
+
     }
 
     private String passwordHash(String pw) {
--- app/src/main/java/de/teambluebaer/patientix/activities/StartActivity.java
+++ app/src/main/java/de/teambluebaer/patientix/activities/StartActivity.java
@@ -3,18 +3,26 @@ package de.teambluebaer.patientix.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
 import de.teambluebaer.patientix.R;
 import de.teambluebaer.patientix.helper.Flasher;
-import de.teambluebaer.patientix.xmlParser.Form;
-import de.teambluebaer.patientix.xmlParser.JavaStrucBuilder;
-import de.teambluebaer.patientix.xmlParser.MetaData;
-import de.teambluebaer.patientix.xmlParser.MetaandForm;
+import de.teambluebaer.patientix.helper.RestfulHelper;
+import static de.teambluebaer.patientix.helper.Constants.TABLET_ID;
 
 /**
  * This class have the patient data...
@@ -24,9 +32,12 @@ public class StartActivity extends Activity {
 
     private Button buttonStart;
     private Button buttonUpdate;
+    private ArrayList<NameValuePair> parameterMap = new ArrayList<NameValuePair>();
+    private int responseCode;
 
-     /**
+    /**
      * In this method is defined what happens on create of the Activity
+     *
      * @param savedInstanceState
      */
     @Override
@@ -47,9 +58,10 @@ public class StartActivity extends Activity {
 
     /**
      * When the start button is clicked, then opens the FormActivity
+     *
      * @param v
      */
-    public void onClickStartButton(View v){
+    public void onClickStartButton(View v) {
         Flasher.flash(buttonStart, "1x3");
         Intent intent = new Intent(StartActivity.this, SignatureActivity.class);
         startActivity(intent);
@@ -57,19 +69,51 @@ public class StartActivity extends Activity {
     }
 
     /**
-     *
      * @param v
      */
-    public void onClickUpdateButton(View v){
+    public void onClickUpdateButton(View v) {
         Flasher.flash(buttonUpdate, "1x5");
+
+
         //TODO SEND "OLD" DATA TO SERVER
 
-        Form.getInstance().refresh();
-        MetaData.getInstance().refresh();
+       // Form.getInstance().refresh();
+        //MetaData.getInstance().refresh();
 
-        MetaandForm metaandform = JavaStrucBuilder.buildStruc();
+        //MetaandForm metaandform = JavaStrucBuilder.buildStruc();
 
-        Toast.makeText(StartActivity.this, "Das Formular ist bereit", Toast.LENGTH_SHORT).show();
+        parameterMap.add(new BasicNameValuePair("tabletID", TABLET_ID));
+
+        //send the request to server
+        RestfulHelper restfulHelper = new RestfulHelper();
+        responseCode = restfulHelper.executeRequest("formula", parameterMap);
+        if (200 == responseCode) {
+            try {
+
+                File folders = new File (Environment.getExternalStorageDirectory().toString()+"/patientix");
+                folders.mkdirs();
+                String extStorageDirectory = folders.toString();
+              //  deleteFile("form.xml");
+                File myFile = new File(extStorageDirectory,"form.xml");
+                myFile.createNewFile();
+                FileOutputStream fOut = new FileOutputStream(myFile);
+                OutputStreamWriter myOutWriter =
+                        new OutputStreamWriter(fOut);
+                myOutWriter.append(restfulHelper.responseString);
+                myOutWriter.close();
+                fOut.close();
+                Toast.makeText(getBaseContext(), "Fragebogen wurde gespeichert!", Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Log.d("FileSaveExeption", e.toString());
+                Toast.makeText(getBaseContext(), "Fehler beim Speichern des Fragebogens", Toast.LENGTH_LONG).show();
+            }
+        } else if(404 == responseCode) {
+            Toast.makeText(StartActivity.this, "Keine Daten für dieses Tablet vorhanden", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(StartActivity.this, "Kein Verbindung zum Server! Fehlercode: " + responseCode, Toast.LENGTH_LONG).show();
+        }
+
 
     }
 
